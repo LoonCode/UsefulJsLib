c3c9cfed8514d51b5a7c178ee21706d9a3fedff3
Loon <LoonCode@users.noreply.github.com>
2016-01-13 11:50:40 +0800

    Update fromUpload.js

--- fromUpload.js
+++ fromUpload.js
@@ -19,3 +19,29 @@
 
 
     });
+// https://stackoverflow.com/questions/5392344/sending-multipart-formdata-with-jquery-ajax    
+    
+var data = new FormData();
+jQuery.each(jQuery('#file')[0].files, function(i, file) {
+    data.append('file-'+i, file);
+});
+    
+
+var opts = {
+    url: 'php/upload.php',
+    data: data,
+    cache: false,
+    contentType: false,
+    processData: false,
+    type: 'POST',
+    success: function(data){
+        alert(data);
+    }
+};
+if(data.fake) {
+    // Make sure no text encoding stuff is done by xhr
+    opts.xhr = function() { var xhr = jQuery.ajaxSettings.xhr(); xhr.send = xhr.sendAsBinary; return xhr; }
+    opts.contentType = "multipart/form-data; boundary="+data.boundary;
+    opts.data = data.toString();
+}
+jQuery.ajax(opts);
